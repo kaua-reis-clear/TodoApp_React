386b85fd203c59a12fe666d4c1f21fa967140e3e
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-29 11:56:22 -0300

    created routes calling firestore crud methods.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const admin = require('firebase-admin');
 const serviceAccount = require('./configs/serviceAccountKey.json');
 const { getFirestore } = require('firebase-admin/firestore')
+const bodyParser = require('body-parser')
 
 const port = 8080;
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,11 +19,12 @@ admin.initializeApp({
 const db = getFirestore();
 
 app.get('/', (req, res) => {
-  db.collection('todo').get().then(data => data.docs).then(docs => res.json(docs))
+  const docs = [];
+  db.collection('todo').get().then(data => data.docs.map(doc => docs.push({id: doc.id, desc: doc.data().desc, done: doc.data().done}))).then(() => res.json(docs));
 });
 
 app.post('/', (req, res) => {
-    db.collection('todo').add({teste: 'testando'}).then(_ => res.send('adicionado'))
+    db.collection('todo').add({desc: req.body.desc, done: false}).then(_ => res.send('adicionado'))
 })
 
 app.listen(port, () => {
